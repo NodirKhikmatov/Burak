ecd0d7d2ce0ccd386f8abbdc7811ed65b312eb67
Nodir8 <Nodirbek8464>
2024-12-09 16:57:18 +0900

    Feat:task H

--- src/train.js
+++ src/train.js
@@ -1,3 +1,18 @@
+
+/*H-TASK: 
+
+shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
+MASALAN: getPositive([1, -4, 2]) return qiladi "12"*/
+
+function getPositive(arr){
+    return arr.filter(num => num > 0).join("");
+}
+
+console.log(getPositive([2,-5,5]));
+
+
+
+
 /*task G
 Yagona parametrga ega function tuzing.
 Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
@@ -8,11 +23,11 @@ Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
 Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.*/
 
 
-function findMax(numbers){
-let highestNumber = Math.max(...numbers);
+// function findMax(numbers){
+// let highestNumber = Math.max(...numbers);
 
-return numbers.indexOf(highestNumber)
-}
-const results = findMax([4,25,89,5])
-console.log(results);
+// return numbers.indexOf(highestNumber)
+// }
+// const results = findMax([4,25,89,5])
+// console.log(results);
 
