48265c0fd4bd8d65693705066fb97bee614445f4
Nodir8 <Nodirbek8464>
2024-12-22 12:36:26 +0900

    integrate middleware session

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,20 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
-console.log("_dirname:", __dirname);
+
+import session from '.express-session';
+import ConnectMongoDB from 'connect-mongodb-session';
+
+const MongoDBStore = connectMongoDB(session){
+    const store = new MongoDBStore({
+        url: String(process.env,MONGO_URL),
+        collection: "sessions",    //mongo collection
+    })
+}
+
+
+
+// console.log("_dirname:", __dirname);
 
 /** 1-entrance */
 const app = express();
@@ -14,6 +27,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 /** 2-sessions */
+app.use(session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+        maxAge:1000 *3600 * 3, //3hours
+    },
+    store:store,
+    resave:true,
+    saveUninitialized: true,  //save uninitialized sessions to the store
+}))
 
 /** 3-views */
 app.set("views", path.join(__dirname, "views"));
