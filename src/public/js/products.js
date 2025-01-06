82e4448fec41ebf4ffe49236948380f4c596d1bc
Nodir8 <Nodirbek8464>
2025-01-07 05:32:21 +0900

    fixed err

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -13,7 +13,7 @@ $(function () {
   });
 
   $("#process-btn").on("click", () => {
-    $(".dish-container").slideToggle(5000);
+    $(".dish-container").slideToggle(500);
     $("#process-btn").css("display", "none");
   });
 
@@ -46,12 +46,12 @@ $(function () {
 });
 
 function validateForm() {
-  const productName = $(".product-name").val();
-  const productPrice = $(".product-price").val();
-  const productLeftCount = $(".product-left-count").val();
-  const productCollection = $(".product-collection").val();
-  const productDesc = $(".product-desc").val();
-  const productStatus = $(".product-status").val();
+  const productName = $(".product-name").val(),
+    productPrice = $(".product-price").val(),
+    productLeftCount = $(".product-left-count").val(),
+    productCollection = $(".product-collection").val(),
+    productDesc = $(".product-desc").val(),
+    productStatus = $(".product-status").val();
 
   if (
     productName === "" ||
@@ -70,9 +70,9 @@ function previewFileHandler(input, order) {
   const imgClassName = input.className;
   console.log("input:", input);
 
-  const file = $(` .${imgClassName}`).get(0).files[0];
-  const fileType = file["type"];
-  const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+  const file = $(` .${imgClassName}`).get(0).files[0],
+    fileType = file["type"],
+    validImageType = ["image/jpg", "image/jpeg", "image/png"];
 
   if (!validImageType.includes(fileType)) {
     alert("Please insert only jpeg, jpeg or png!");
