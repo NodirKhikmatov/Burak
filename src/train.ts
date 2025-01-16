e51e4ec0744db611df5b9163d965e2c9d5b3855c
Nodir8 <Nodirbek8464>
2025-01-16 17:27:13 +0900

    Task T

--- src/train.ts
+++ src/train.ts
@@ -1,8 +1,47 @@
-/* TASK-S:
+/**
+ TASK-T:
+
+Shunday function yozing, u sonlardan tashkil topgan 2 ta array qabul qilsin va ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin
+MASALAN: mergeSortedArrays([0,3,4,31], [4,6,30]); return [0,3,4,4,6,30,31]
+
+ */
+function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
+  const mergedArray: number[] = [];
+  let i = 0; 
+  let j = 0;
+
+  
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      mergedArray.push(arr1[i]);
+      i++;
+    } else {
+      mergedArray.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    mergedArray.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    mergedArray.push(arr2[j]);
+    j++;
+  }
+
+  return mergedArray;
+}
+
+console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+
+
+/* TASK-
 
 Shunday function yozing, u numberlardan tashkil topgan array qabul qilsin va osha numberlar orasidagi tushib qolgan sonni topib uni return qilsin
 MASALAN: missingNumber([3, 0, 1]) return 2
- */
+
 
 function missingNumber(str: number[]) {
   const a = str.sort();
@@ -13,8 +52,8 @@ function missingNumber(str: number[]) {
   }
 }
 
-console.log(missingNumber([3, 5, 2, 1]));
-/*TASK R
+console.log(missingNumber([3, 5, 2, 1]));*/
+/*TASK R 
 
 Shunday function yozing, u string parametrga ega bo'lsin.
 Agar argument sifatida berilayotgan string, "1 + 2" bo'lsa,
