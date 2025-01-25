c560721a2daeadd3990a2b80023258a35ea6568d
Nodir8 <Nodirbek8464>
2025-01-25 16:49:14 +0900

    fix: modify getmemberDetail businessLogic

--- src/models/member.service.ts
+++ src/models/member.service.ts
@@ -93,6 +93,20 @@ class MemberService {
     }
   }
 
+  public async getMemberDetail(member: Member): Promise<Member> {
+    const memberId = shapeIntoMongooseObjectId(member._id);
+    const result = await this.memberModel
+      .findOne({
+        _id: memberId,
+        memberStatus: MemberStatus.ACTIVE,
+      })
+      .exec();
+
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result;
+  }
+
   public async processLogin(input: LoginInput): Promise<Member> {
     const member = await this.memberModel
       .findOne(
